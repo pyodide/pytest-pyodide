8f43be14bef1795489f47b5fcad7d5c4de0a4783
Gyeongjae Choi <def6488@gmail.com>
2022-08-02 02:12:10 +0000

    Add test

---
+++ tests/test_marker.py
@@ -0,0 +1,11 @@
+import pytest
+
+from pytest_pyodide import run_in_pyodide
+
+
+@pytest.mark.xfail_browsers(
+    node="Should xfail", firefox="Should xfail", chrome="Should xfail"
+)
+@run_in_pyodide
+def test_xfail_browser(selenium):
+    raise AssertionError("This test should xfail")
