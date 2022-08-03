665f794d70c44bb328342ee95feeb736eb673a2d
Gyeongjae Choi <def6488@gmail.com>
2022-08-03 09:18:21 +0900

    Rename wrappers (#14)

--- pytest_pyodide/__init__.py
+++ pytest_pyodide/__init__.py
@@ -1,17 +1,21 @@
 from importlib.metadata import PackageNotFoundError, version
 
-from .browser import (
+from .decorator import run_in_pyodide
+from .runner import (  # deprecated
     BrowserWrapper,
+    NodeRunner,
     NodeWrapper,
+    PlaywrightChromeRunner,
     PlaywrightChromeWrapper,
+    PlaywrightFirefoxRunner,
     PlaywrightFirefoxWrapper,
     PlaywrightWrapper,
+    SeleniumChromeRunner,
     SeleniumChromeWrapper,
+    SeleniumFirefoxRunner,
     SeleniumFirefoxWrapper,
     SeleniumWrapper,
 )
-from .decorator import run_in_pyodide
-from .fixture import *  # noqa: F403, F401
 from .server import spawn_web_server
 from .utils import parse_driver_timeout, set_webdriver_script_timeout
 
@@ -22,6 +26,16 @@ except PackageNotFoundError:
     pass
 
 __all__ = [
+    "NodeRunner",
+    "PlaywrightChromeRunner",
+    "PlaywrightFirefoxRunner",
+    "SeleniumChromeRunner",
+    "SeleniumFirefoxRunner",
+    "set_webdriver_script_timeout",
+    "parse_driver_timeout",
+    "run_in_pyodide",
+    "spawn_web_server",
+    # deprecated
     "BrowserWrapper",
     "SeleniumWrapper",
     "PlaywrightWrapper",
@@ -30,8 +44,4 @@ __all__ = [
     "PlaywrightChromeWrapper",
     "PlaywrightFirefoxWrapper",
     "NodeWrapper",
-    "set_webdriver_script_timeout",
-    "parse_driver_timeout",
-    "run_in_pyodide",
-    "spawn_web_server",
 ]
