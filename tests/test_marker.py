eb15f5b53c0eadbabded8828ae74f532e095332a
Gyeongjae Choi <def6488@gmail.com>
2022-08-03 05:00:28 +0000

    Fix xfail test

--- tests/test_marker.py
+++ tests/test_marker.py
@@ -4,7 +4,10 @@ from pytest_pyodide import run_in_pyodide
 
 
 @pytest.mark.xfail_browsers(
-    node="Should xfail", firefox="Should xfail", chrome="Should xfail"
+    node="Should xfail",
+    firefox="Should xfail",
+    chrome="Should xfail",
+    safari="Should xfail",
 )
 @run_in_pyodide
 def test_xfail_browser(selenium):
