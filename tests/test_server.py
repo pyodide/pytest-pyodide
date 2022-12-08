6fb897377d448a2141c682c00501b13b189b1dea
koenvo <info@koenvossen.nl>
2022-12-09 00:36:45 +0100

    Add request handler spawn web server (#47)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,4 +1,6 @@
+import http.server
 import urllib.request
+from http import HTTPStatus
 
 from pytest_pyodide.server import spawn_web_server
 
@@ -18,3 +20,18 @@ def test_spawn_web_server_with_params(tmp_path):
         assert res.read() == b"a"
         assert res.headers["Access-Control-Allow-Origin"] == "*"
         assert res.headers.get("Custom-Header") == "42"
+
+
+class HelloWorldHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(HTTPStatus.OK)
+        self.end_headers()
+        self.wfile.write(b"hello world")
+
+
+def test_custom_handler(tmp_path):
+    with spawn_web_server(tmp_path, handler_cls=HelloWorldHandler) as server:
+        hostname, port, _ = server
+        res = urllib.request.urlopen(f"http://{hostname}:{port}/index.txt")
+        assert res.status == 200
+        assert res.read() == b"hello world"
