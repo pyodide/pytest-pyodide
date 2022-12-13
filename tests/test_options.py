6c57a23b2904a0fd305deb0b98af1fd238517f44
ryanking13 <def6488@gmail.com>
2022-12-13 07:15:37 +0000

    Add more tests

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,13 +1,47 @@
+import pytest
+
+
 def test_dist_dir(pytester):
 
     dist_dir = "dist"
 
     pytester.makepyfile(
         f"""
-        def test_dist_dir():
-            assert pytest.pyodide_dist_dir == {dist_dir!r}
+        import pytest
+        def test_dist_dir(request):
+            assert request.config.option.getoption("--dist-dir") == {dist_dir!r}
         """
     )
 
     result = pytester.runpytest("--dist-dir", dist_dir)
     result.assert_outcomes(passed=1)
+
+
+@pytest.mark.parametrize("runner", ["selenium", "playwright"])
+def test_runner(pytester, runner):
+    pytester.makepyfile(
+        f"""
+        import pytest
+        def test_dist_dir(request):
+            assert request.config.option.getoption("--runner") == {runner!r}
+        """
+    )
+
+    result = pytester.runpytest("--runner", runner)
+    result.assert_outcomes(passed=1)
+
+
+def test_invalid_runner(pytester):
+
+    runner = "blah"
+
+    pytester.makepyfile(
+        f"""
+        import pytest
+        def test_dist_dir(request):
+            assert request.config.option.getoption("--runner") == {runner!r}
+        """
+    )
+
+    result = pytester.runpytest("--runner", runner)
+    result.assert_outcomes(failed=1)
