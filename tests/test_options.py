769f8bdba91b35bbb147cc71e56b63467ba90b75
Gyeongjae Choi <def6488@gmail.com>
2022-12-15 06:09:04 +0000

    Fix test

--- tests/test_options.py
+++ tests/test_options.py
@@ -93,5 +93,6 @@ def test_invalid_runtime(pytester, _runtime):
         """
     )
 
-    result = pytester.runpytest("--runtime", _runtime)
-    result.assert_outcomes(errors=1)
+    # TODO: catch internal errors directly?
+    with pytest.raises(ValueError, match="Pytest terminal summary report not found"):
+        pytester.runpytest("--runtime", _runtime)
