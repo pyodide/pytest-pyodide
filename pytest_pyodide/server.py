b292fca37da69dd381391363e7ce8dd937e662dc
ryanking13 <def6488@gmail.com>
2023-04-21 04:38:43 +0000

    Expose DefaultHandler

--- pytest_pyodide/server.py
+++ pytest_pyodide/server.py
@@ -23,6 +23,56 @@ def _default_templates() -> dict[str, bytes]:
     return templates
 
 
+class DefaultHandler(http.server.SimpleHTTPRequestHandler):
+    default_templates = _default_templates()
+
+    def __init__(self, *args, **kwargs):
+        self.extra_headers = kwargs.pop("extra_headers", {})
+        super().__init__(*args, **kwargs)
+
+    def log_message(self, format_, *args):
+        print(
+            "[%s] source: %s:%s - %s"
+            % (
+                self.log_date_time_string(),
+                *self.client_address,
+                format_ % args,
+            )
+        )
+
+    def get_template(self, path: str) -> bytes | None:
+        """
+        Return the content of the template if it exists, None otherwise
+
+        This method is used to serve the default templates, and can be
+        overridden to serve custom templates.
+        """
+        return self.default_templates.get(path)
+
+    def do_GET(self):
+        body = self.get_template(self.path)
+        if body:
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.send_header("Content-Length", str(len(body)))
+            self.end_headers()
+
+            self.copyfile(BytesIO(body), self.wfile)
+        else:
+            return super().do_GET()
+
+    def end_headers(self):
+        # Enable Cross-Origin Resource Sharing (CORS)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        for k, v in self.extra_headers.items():
+            self.send_header(k, v)
+        if len(self.extra_headers) > 0:
+            joined_headers = ",".join(self.extra_headers.keys())
+            # if you don't send this, CORS blocks custom headers in javascript
+            self.send_header("Access-Control-Expose-Headers", joined_headers)
+        super().end_headers()
+
+
 @contextlib.contextmanager
 def spawn_web_server(dist_dir, extra_headers=None, handler_cls=None):
     if not extra_headers:
@@ -68,43 +118,7 @@ def run_web_server(q, log_filepath, dist_dir, extra_headers, handler_cls):
     sys.stderr = log_fh
 
     if not handler_cls:
-        default_templates = _default_templates()
-
-        class DefaultHandler(http.server.SimpleHTTPRequestHandler):
-            def log_message(self, format_, *args):
-                print(
-                    "[%s] source: %s:%s - %s"
-                    % (
-                        self.log_date_time_string(),
-                        *self.client_address,
-                        format_ % args,
-                    )
-                )
-
-            def do_GET(self):
-                if self.path in default_templates:
-                    body = default_templates[self.path]
-                    self.send_response(200)
-                    self.send_header("Content-type", "text/html; charset=utf-8")
-                    self.send_header("Content-Length", str(len(body)))
-                    self.end_headers()
-
-                    self.copyfile(BytesIO(body), self.wfile)
-                else:
-                    return super().do_GET()
-
-            def end_headers(self):
-                # Enable Cross-Origin Resource Sharing (CORS)
-                self.send_header("Access-Control-Allow-Origin", "*")
-                for k, v in extra_headers.items():
-                    self.send_header(k, v)
-                if len(extra_headers) > 0:
-                    joined_headers = ",".join(extra_headers.keys())
-                    # if you don't send this, CORS blocks custom headers in javascript
-                    self.send_header("Access-Control-Expose-Headers", joined_headers)
-                super().end_headers()
-
-        handler_cls = DefaultHandler
+        handler_cls = functools.partial(DefaultHandler, extra_headers=extra_headers)
 
     with socketserver.TCPServer(("", 0), handler_cls) as httpd:
         host, port = httpd.server_address
