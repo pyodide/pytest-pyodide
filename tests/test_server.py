8c4a468d8cbf9c5ed0f436eea4a8dac7b809ee63
Gyeongjae Choi <def6488@gmail.com>
2023-05-10 10:04:15 +0900

    Embed default html templates (#87)
    This embeds test.html and module_test.html into pytest-pyodide wheel. So we don't need to bundle them into pyodide distribution.

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,8 +1,9 @@
 import http.server
-import urllib.request
 from http import HTTPStatus
 
-from pytest_pyodide.server import spawn_web_server
+import requests
+
+from pytest_pyodide.server import DefaultHandler, _default_templates, spawn_web_server
 
 
 def test_spawn_web_server_with_params(tmp_path):
@@ -13,12 +14,52 @@ def test_spawn_web_server_with_params(tmp_path):
         port,
         log_path,
     ):
-        res = urllib.request.urlopen(f"http://{hostname}:{port}/index.txt")
-        assert res.status == 200
+        res = requests.get(f"http://{hostname}:{port}/index.txt")
+        assert res.ok
         assert res.headers
-        assert res.read() == b"a"
+        assert res.content == b"a"
         assert res.headers["Access-Control-Allow-Origin"] == "*"
         assert res.headers.get("Custom-Header") == "42"
+
+
+def test_spawn_web_server_default_templates(tmp_path):
+    default_templates = _default_templates()
+
+    with spawn_web_server(tmp_path) as (hostname, port, _):
+        for path, content in default_templates.items():
+            res = requests.get(f"http://{hostname}:{port}{path}")
+            assert res.ok
+            assert res.headers
+            assert res.content == content
+            assert res.headers["Access-Control-Allow-Origin"] == "*"
+
+
+class CustomTemplateHandler(DefaultHandler):
+    def get_template(self, path: str) -> bytes | None:
+        if path == "/index.txt":
+            return b"hello world"
+
+        return super().get_template(path)
+
+
+def test_spawn_web_server_custom_templates(tmp_path):
+    default_templates = _default_templates()
+
+    with spawn_web_server(tmp_path, handler_cls=CustomTemplateHandler) as (
+        hostname,
+        port,
+        _,
+    ):
+        for path, content in default_templates.items():
+            res = requests.get(f"http://{hostname}:{port}{path}")
+            assert res.ok
+            assert res.headers
+            assert res.content == content
+            assert res.headers["Access-Control-Allow-Origin"] == "*"
+
+        res = requests.get(f"http://{hostname}:{port}/index.txt")
+        assert res.ok
+        assert res.content == b"hello world"
 
 
 class HelloWorldHandler(http.server.SimpleHTTPRequestHandler):
@@ -31,6 +72,6 @@ class HelloWorldHandler(http.server.SimpleHTTPRequestHandler):
 def test_custom_handler(tmp_path):
     with spawn_web_server(tmp_path, handler_cls=HelloWorldHandler) as server:
         hostname, port, _ = server
-        res = urllib.request.urlopen(f"http://{hostname}:{port}/index.txt")
-        assert res.status == 200
-        assert res.read() == b"hello world"
+        res = requests.get(f"http://{hostname}:{port}/index.txt")
+        assert res.ok
+        assert res.content == b"hello world"
