pyodide/pytest-pyodide

commit 4f9c1545b0144ccdb5dca19063954533cb24fb5f

ENH Add rename_fixture and standalone decorators (#101)
We use a bunch of different selenium variants but for the most part they are
drop-in replacements for each other. For instance, when using `selenium_standalone`,
we always just say `selenium = selenium_standalone` at the top of the function. I
think it's clearer if we use a decorator to rename the fixture.

pytest_pyodide/hook.py
@@ -170,6 +170,21 @@ def pytest_generate_tests(metafunc: Any) -> None:
         metafunc.parametrize("runtime", pytest.pyodide_runtimes, scope="module")
 
 
+STANDALONE_FIXTURES = [
+    "selenium_standalone",
+    "selenium_standalone_noload",
+    "selenium_webworker_standalone",
+]
+
+
+def _has_standalone_fixture(item):
+    for fixture in item._request.fixturenames:
+        if fixture in STANDALONE_FIXTURES:
+            return True
+
+    return False
+
+
 def pytest_collection_modifyitems(items: list[Any]) -> None:
     # if we are running tests in pyodide, then run all tests for each runtime
     if len(items) > 0 and items[0].config.option.run_in_pyodide:
@@ -186,28 +201,14 @@ def pytest_collection_modifyitems(items: list[Any]) -> None:
     # Since Safari doesn't support more than one simultaneous session, we run all
     # selenium_standalone Safari tests first. We preserve the order of other
     # tests.
-
     OFFSET = 10000
-
     counter = [0]
-    standalone_fixtures = [
-        "selenium_standalone",
-        "selenium_standalone_noload",
-        "selenium_webworker_standalone",
-    ]
-
-    def _has_standalone_fixture(fixturenames):
-        for fixture in fixturenames:
-            if fixture in standalone_fixtures:
-                return True
-
-        return False
 
     def _get_item_position(item):
         counter[0] += 1
         if any(
             [re.match(r"^safari[\-$]?", el) for el in item.keywords._markers.keys()]
-        ) and _has_standalone_fixture(item._request.fixturenames):
+        ) and _has_standalone_fixture(item):
             return counter[0] - OFFSET
         return counter[0]
 

pytest_pyodide/fixture.py
@@ -1,4 +1,6 @@
 import contextlib
+import functools
+import inspect
 import os
 from pathlib import Path
 from typing import Any
@@ -115,6 +117,31 @@ def selenium_common(
         yield runner
     finally:
         runner.quit()
+
+
+def rename_fixture(orig_name, new_name):
+    def use_variant(f):
+        sig = inspect.signature(f)
+        new_params = []
+        for p in sig.parameters.values():
+            if p.name == orig_name:
+                p = p.replace(name=new_name)
+            new_params.append(p)
+        new_sig = sig.replace(parameters=new_params)
+
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            if new_name in kwargs:
+                kwargs[orig_name] = kwargs.pop(new_name)
+            return f(*args, **kwargs)
+
+        wrapper.__signature__ = new_sig  # type:ignore[attr-defined]
+        return wrapper
+
+    return use_variant
+
+
+standalone = rename_fixture("selenium", "selenium_standalone")
 
 
 @pytest.fixture(scope="function")

tests/test_fixture.py
@@ -1,6 +1,8 @@
 import pytest
 
 from pytest_pyodide.decorator import run_in_pyodide
+from pytest_pyodide.fixture import rename_fixture
+from pytest_pyodide.hook import _has_standalone_fixture
 
 
 @pytest.mark.parametrize("dummy", [1, 2, 3])
@@ -24,3 +26,56 @@ def test_playwright_browsers(playwright_browsers, request):
     runtimes = pytest.pyodide_runtimes
 
     assert set(playwright_browsers.keys()) == set(runtimes)
+
+
+@rename_fixture("myfixture", "myfixture_variant")
+def myfunc(a, myfixture):
+    return [a, myfixture]
+
+
+def test_rename_fixture1():
+    assert myfunc(2, 3) == [2, 3]
+    assert myfunc(2, myfixture_variant=3) == [2, 3]
+    assert myfunc(a=2, myfixture_variant=3) == [2, 3]
+
+
+@pytest.fixture
+def myfixture():
+    yield 27
+
+
+@pytest.fixture
+def myfixture_variant():
+    yield 99
+
+
+@rename_fixture("myfixture", "myfixture_variant")
+def test_rename_fixture2(myfixture):
+    assert myfixture == 99
+
+
+def test_has_standalone_fixture(pytester):
+    from textwrap import dedent
+
+    pytester.makepyfile(
+        dedent(
+            """
+            from pytest_pyodide.fixture import rename_fixture
+
+            @rename_fixture("selenium", "selenium_standalone")
+            def test_example1(selenium):
+                pass
+
+            @rename_fixture("selenium_standalone", "selenium_standalone1")
+            def test_example2(selenium_standalone):
+                pass
+            """
+        )
+    )
+    node = pytester.getpathnode("test_has_standalone_fixture.py")
+    r = node.collect()
+    t1, t2 = r
+    assert "test_example1" in t1.name
+    assert "test_example2" in t2.name
+    assert _has_standalone_fixture(t1)
+    assert not _has_standalone_fixture(t2)

commit e253295606a97f6e5d792139e6760a52482e1167

Prevent from overwriting global configurations (#105)
This fixes a test failure that started to occur after #101.

```
! _pytest.outcomes.Exit: playwright failed to launch
```

The cause of the problem was that the combination of the `pytester` fixture and the `pytest_configure` hook is not very good.
We use `pytest_configure` to manage global settings, but this function is called again when we run sub-tests using pytester, resulting in new global settings being overwritten.

So I added a check to prevent overwriting global settings once it is set.

pytest_pyodide/hook.py
@@ -92,9 +92,15 @@ def pytest_configure(config):
     )
 
     run_host, runtimes = _filter_runtimes(config.option.runtime)
-    pytest.pyodide_run_host_test = run_host
-    pytest.pyodide_runtimes = runtimes
-    pytest.pyodide_dist_dir = config.getoption("--dist-dir")
+
+    # using `pytester` fixture seems to call this hook again with different options
+    # so this is a workaround to avoid overwriting the values
+    if not hasattr(pytest, "pyodide_run_host_test"):
+        pytest.pyodide_run_host_test = run_host
+    if not hasattr(pytest, "pyodide_runtimes"):
+        pytest.pyodide_runtimes = runtimes
+    if not hasattr(pytest, "pyodide_dist_dir"):
+        pytest.pyodide_dist_dir = config.getoption("--dist-dir")
 
 
 @pytest.hookimpl(tryfirst=True)

tests/test_options.py
@@ -1,5 +1,7 @@
 import pytest
 
+from pytest_pyodide.hook import _filter_runtimes
+
 
 def test_dist_dir(pytester):
     dist_dir = "dist"
@@ -50,42 +52,17 @@ def test_invalid_runner(pytester):
 @pytest.mark.parametrize(
     "_runtime",
     [
-        "chrome",
-        "firefox",
-        "safari",
-        "node",
-        "firefox,chrome",
-    ],
-)
-def test_runtime(pytester, _runtime):
-    runtimes = _runtime.split(",")
-
-    pytester.makepyfile(
-        f"""
-        import pytest
-        def test_option():
-            assert pytest.pyodide_runtimes == set({runtimes!r})
-        """
-    )
-
-    result = pytester.runpytest("--runtime", _runtime)
-    result.assert_outcomes(passed=1)
-
-
-@pytest.mark.parametrize(
-    "_runtime",
-    [
         "invalid",
     ],
 )
 def test_invalid_runtime(pytester, _runtime):
-    runtimes = _runtime.split(",")
+    _runtime.split(",")
 
     pytester.makepyfile(
-        f"""
+        """
         import pytest
         def test_option():
-            assert pytest.pyodide_runtimes == set({runtimes!r})
+            assert True
         """
     )
 
@@ -93,3 +70,19 @@ def test_invalid_runtime(pytester, _runtime):
     with pytest.raises(ValueError, match="Pytest terminal summary report not found"):
         result = pytester.runpytest("--runtime", _runtime)
         result.assert_outcomes(errors=1)
+
+
+@pytest.mark.parametrize(
+    "_runtime,expected",
+    [
+        ("chrome", (True, {"chrome"})),
+        ("firefox", (True, {"firefox"})),
+        ("node", (True, {"node"})),
+        ("chrome,firefox", (True, {"chrome", "firefox"})),
+        ("chrome-no-host,firefox", (False, {"chrome", "firefox"})),
+        ("host", (True, set())),
+        ("chrome-no-host,host", (True, {"chrome"})),
+    ],
+)
+def test_filter_runtimes(_runtime, expected):
+    _filter_runtimes(_runtime) == expected