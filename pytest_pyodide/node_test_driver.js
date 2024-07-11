b528c1edfe5106567203fcfcd4cbe699ef566225
Joe Marshall <joe.marshall@nottingham.ac.uk>
2024-07-11 16:46:41 +0100

    strip whitespace from received lines

--- pytest_pyodide/node_test_driver.js
+++ pytest_pyodide/node_test_driver.js
@@ -55,6 +55,12 @@ rl.on("line", async function (line) {
     return;
   }
   if (line !== cur_uuid) {
+    if(line.endsWith("\n")){
+      line=line.substring(0,line.length()-1)
+    }
+    if(line.endsWith("\r")){
+      line=line.substring(0,line.length()-1)
+    }
     if( line.trim() === ""){
       cur_code += "\n";
     }else{
